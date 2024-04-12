3f2fe706327cd95a235a063cc7f538598d4e1672
ghmarconato <ghmarconato@gmail.com>
2024-04-12 00:00:39 -0300

    Load Videos and Prompts

--- upload-ai-api/src/routes/upload-video.ts
+++ upload-ai-api/src/routes/upload-video.ts
@@ -2,7 +2,12 @@ import { FastifyInstance } from "fastify";
 import {fastifyMultipart} from "@fastify/multipart"
 import { prisma } from "../lib/prisma";
 import path from "node:path";
+import fs from 'node:fs'
+import {pipeline} from 'node:stream'
+import {promisify} from 'node:util'
 import {randomUUID} from 'node:crypto'
+
+const pump = promisify(pipeline)
 
 export async function uploadVideoRoute(app: FastifyInstance){
     app.register(fastifyMultipart, {
@@ -30,6 +35,15 @@ export async function uploadVideoRoute(app: FastifyInstance){
 
         const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
+        await pump(data.file, fs.createWriteStream(uploadDestination))
+
+        const video = await prisma.video.create({
+            data:{
+                name: data.filename,
+                path: uploadDestination,
+            }
+        })
         
+        return {video,}
     })
 }
