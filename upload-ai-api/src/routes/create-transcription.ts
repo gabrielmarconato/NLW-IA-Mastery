4194a52b3ad90425afa9a13a46e7bf41ee8fb357
ghmarconato <ghmarconato@gmail.com>
2024-04-12 17:41:39 -0300

    API corrections

--- upload-ai-api/src/routes/create-transcription.ts
+++ upload-ai-api/src/routes/create-transcription.ts
@@ -1,51 +1,59 @@
-import {FastifyInstance} from "fastify";
-import {z} from 'zod'
-import {prisma} from "../lib/prisma";
-import {createReadStream} from 'node:fs'
+import { FastifyInstance } from "fastify";
+import { createReadStream } from "node:fs";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
-export async function createTranscriptionRoute(app: FastifyInstance){
-    app.post('/videos/:videoId/transcription', async (req,res) => {
-        const paramsSchema = z.object({
-            videoId: z.string().uuid(),
-        })
-        
-        const { videoId } = paramsSchema.parse(req.params)
-
-        const bodySchema = z.object({
-            prompt: z.string().nullish(),
-        })
-
-        const { prompt } = bodySchema.parse(req.body)
-
-        console.log(prompt)
-
-        const video = await prisma.video.findUniqueOrThrow({
-            where:{
-                id: videoId,
-            }
-        })
-
-        // const { data } = await axios.get(video.path, {
-        //     responseType: 'arraybuffer',
-        //   });
-        // const file = await toFile(Buffer.from(data), 'teste2.mp3');
-
-
-        const videoPath = video.path
-
-        console.log(videoPath)
-
-        const audioReadStream = createReadStream(videoPath)
-
-        const response = await openai.audio.transcriptions.create({
-            file: audioReadStream,
-            model:'whisper-1',
-            language: 'pt',
-            response_format:'json',
-            temperature: 0,
-        })
-
-        return response.text
+export async function createTranscriptionRoute(app: FastifyInstance) {
+  app.post('/videos/:videoId/transcription', async (req) => {
+    const paramsSchema = z.object({
+      videoId: z.string().uuid(),
     })
-}
+
+    const { videoId } = paramsSchema.parse(req.params)
+
+    const bodySchema = z.object({
+      prompt: z.string(),
+    })
+
+    const { prompt } = bodySchema.parse(req.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    const videoPath = video.path
+    const audioReadStream = createReadStream(videoPath)
+
+    try{
+        const response = await openai.audio.transcriptions.create({
+        file: audioReadStream,
+        model: 'whisper-1',
+        language: 'pt',
+        response_format: 'json',
+        temperature: 0,
+        prompt,
+      })}catch(error){
+        console.log(error)
+        return error
+      }
+    
+
+    return response.text
+  })}
+//     await prisma.video.update({
+//       where: {
+//         id: videoId,
+//       },
+//       data: {
+//         transcription,
+//       }
+//     })
+
+//     return {
+//       transcription,
+//     }
+//   })
+//}
